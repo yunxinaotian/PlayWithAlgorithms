0add69e1243738ad66daf965e6e61018ac8d912a
lienshun <lnsh@outlook.com>
2017-06-08 15:26:23 +0800

    优化插入排序，及实验插入排序近似有序的排序时间

--- InsertionSort/InsertionSort/main.cpp
+++ InsertionSort/InsertionSort/main.cpp
@@ -9,17 +9,20 @@ template<typename T>
 void insertionSort(T arr[], int n) {
 	for (int i = 0; i < n; i++) {
 		//寻找元素arr[i]合适的插入位置
-		for (int j = i; j > 0 && arr[j] < arr[j-1]; j--){
-			swap(arr[j], arr[j-1]);
-
+		T e = arr[i];
+		int j;//保存元素e应该插入的位置
+		for (j = i; j > 0 && arr[j-1] > e; j--){
+			arr[j] = arr[j-1];
 		}
+		arr[j] = e;
 	}
 }
 
 int main() {
 
 	int n = 10000;
-	int *arr = SortTestHelper::generateRandomArray(n, 0, n);
+	//int *arr = SortTestHelper::generateRandomArray(n, 0, n);
+	int *arr = SortTestHelper::generateNearlyOrderedArray(n, 10);
 	int *arr2 = SortTestHelper::copyIntArray(arr, n);
 	SortTestHelper::testSort("Insertion Sort", insertionSort, arr, n);
 	SortTestHelper::testSort("Selection Sort", selectionSort, arr2, n);
