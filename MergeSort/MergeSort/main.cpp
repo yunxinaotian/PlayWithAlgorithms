4d61d7857f399cb0bf7b72de4e9f255dc032b387
lienshun <lnsh@outlook.com>
2017-06-08 19:15:24 +0800

    优化归并排序利用插入排序的优点

--- MergeSort/MergeSort/main.cpp
+++ MergeSort/MergeSort/main.cpp
@@ -41,12 +41,18 @@ void __merge(T arr[], int l, int mid, int r) {
 template<typename T>
 void __mergeSort(T arr[], int l, int r) {
 
-	if (l >= r)
+	//if (l >= r)
+	//	return;
+	if (r - l <= 15) {
+		insertionSort(arr, l, r);
 		return;
+	}
+
 	int mid = (l + r) / 2;
 	__mergeSort(arr, l, mid);
 	__mergeSort(arr, mid + 1, r);
-	__merge(arr, l, mid, r);
+	if( arr[mid] > arr[mid+1] )
+		__merge(arr, l, mid, r);
 }
 
 template<typename T>
